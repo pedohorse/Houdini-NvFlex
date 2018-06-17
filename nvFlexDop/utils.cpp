5f66101a38db687edbedb5f4caa019c413c4a89c
pedohorse <xapkohheh@gmail.com>
2018-06-17 10:55:09 -0400

    logging level set from environment

--- nvFlexDop/utils.cpp
+++ nvFlexDop/utils.cpp
@@ -3,13 +3,21 @@
 
 #include "utils.h"
 
-
+static unsigned short msglevel = 2;
 
 void messageLog(unsigned short level, const char* fmt, ...) {
-	if (level > 999)return;
+	if (level > msglevel)return;
 	va_list args;
 	va_start(args, fmt);
 	std::vfprintf(stderr, fmt, args);
 	std::fflush(stderr);
 	va_end(args);
+}
+
+void setMessageLogLevel(unsigned short level) {
+	msglevel = level;
+}
+
+short getMessageLogLevel() {
+	return msglevel;
 }
